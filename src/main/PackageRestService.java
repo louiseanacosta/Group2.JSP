1beaae1190b5862066151b385591bb2cb9cbb936
Colin <colin.lee.chee@gmail.com>
2019-06-25 11:30:18 -0600

    Update PackageRestService.java
    Updated package REST service

--- src/main/PackageRestService.java
+++ src/main/PackageRestService.java
@@ -1,5 +1,44 @@
 package main;
 
-public class PackageRestService {
+import java.lang.reflect.Type;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+
+import model.Package;
+
+@Path("package")
+
+public class PackageRestService {
+	
+		@GET
+		@Path("/getpackage/{packageId}")
+		@Produces(MediaType.APPLICATION_JSON)
+		public String getpackage(@PathParam("packageId") int packageId)
+		{
+			//http://localhost:8080/Group2JSP/rest/package/getpackage/1
+			EntityManager em =
+					Persistence.createEntityManagerFactory("Group2JSP").createEntityManager();
+			Query query = em.createQuery("SELECT p FROM Package p where p.packageId ="+ packageId);
+			Package package1 = (Package) query.getSingleResult();
+			Gson gson = new Gson();
+			Type type = new TypeToken<Package>(){}.getType();
+			String jsonString = gson.toJson(package1, type);
+			em.close();
+			return jsonString;
+		}
+		
 }
+		
+		
+		
